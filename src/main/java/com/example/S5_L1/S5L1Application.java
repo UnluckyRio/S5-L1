67131ad02eafabd14f4b79c212ed16aa3d71ec64
Valerio Di Felice <valesmile.99@gmail.com>
2025-10-06 11:54:17 +0200

    Initial commit

---
+++ src/main/java/com/example/S5_L1/S5L1Application.java
@@ -0,0 +1,13 @@
+package com.example.S5_L1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5L1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/S5_L1/S5L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.S5_L1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
